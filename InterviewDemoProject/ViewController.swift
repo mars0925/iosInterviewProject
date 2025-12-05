a553f160d06f1307e7e2d8c8f527b36127673572
mars0925 <mars09250148@gmail.com>
2025-12-05 09:50:45 +0800

    Initial Commit

---
+++ InterviewDemoProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InterviewDemoProject
+//
+//  Created by MarsChang on 2025/12/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
